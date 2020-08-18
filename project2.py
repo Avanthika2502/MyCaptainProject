9c831d622b118461cb7cad6647f223015cf8f91f
Avanthika2502 <69524045+Avanthika2502@users.noreply.github.com>
2020-08-18 19:27:03 +0530

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,20 @@
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0]) 
+print(mylist[1]) 
+print(mylist[2]) 
+
+myTuple=("c","c++","java","python")
+print(myTuple)
+
+mydict={
+    "apple": "red",
+    "banana":"yellow",
+    "grapes":"green"
+    }
+
+
+del mydict["apple"]
+print(mydict)
